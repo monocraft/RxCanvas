1f8e3d0b878a3cb87863855fe725137302faaa92
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-06-12 23:58:22 +0200

    Added rectangle editor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -397,6 +397,104 @@ namespace RxCanvas
         }
     }
 
+    public class PortableXCanvasRectangleEditor : IEditor, IDisposable
+    {
+        public enum State { None, TopLeft, TopRight, BottomLeft, BottomRight }
+
+        public bool IsEnabled { get; set; }
+
+        private ICanvas _canvas;
+        private IRectangle _xrectangle;
+        private IRectangle _rectangle;
+        private State _state = State.None;
+        private IDisposable _downs;
+        private IDisposable _drag;
+        private ImmutablePoint _start;
+
+        public PortableXCanvasRectangleEditor(ICanvas canvas)
+        {
+            _canvas = canvas;
+
+            var dragMoves = from move in _canvas.Moves
+                            where _canvas.IsCaptured
+                            select move;
+
+            var allPositions = Observable.Merge(_canvas.Downs, _canvas.Ups, dragMoves);
+
+            var dragPositions = from move in allPositions
+                                select move;
+
+            _downs = _canvas.Downs.Subscribe(p =>
+            {
+                if (!IsEnabled)
+                {
+                    return;
+                }
+
+                if (_canvas.IsCaptured)
+                {
+                    UpdatePositionAndSize(p);
+                    _state = State.None;
+                    _canvas.ReleaseCapture();
+                }
+                else
+                {
+                    _start = p;
+                    // TODO: Use IoC container to get XRectangle as IRectangle.
+                    _xrectangle = new XRectangle()
+                    {
+                        X = p.X,
+                        Y = p.Y,
+                        Width = 0.0,
+                        Height = 0.0,
+                        Stroke = new XColor(0xFF, 0x00, 0x00, 0x00),
+                        StrokeThickness = 2.0,
+                        Fill = new XColor(0x00, 0xFF, 0xFF, 0xFF),
+                        IsFilled = false
+                    };
+                    // TODO: Use IoC container to get WpfRectangle as IRectangle.
+                    _rectangle = new WpfRectangle(_xrectangle);
+                    _canvas.Add(_rectangle);
+                    _canvas.Capture();
+                    _state = State.BottomRight;
+                }
+            });
+
+            _drag = dragPositions.Subscribe(p =>
+            {
+                if (!IsEnabled)
+                {
+                    return;
+                }
+
+                if (_state == State.BottomRight)
+                {
+                    UpdatePositionAndSize(p);
+                }
+            });
+        }
+
+        private void UpdatePositionAndSize(ImmutablePoint p)
+        {
+            double width = Math.Abs(p.X - _start.X);
+            double height = Math.Abs(p.Y - _start.Y);
+            _xrectangle.X = Math.Min(_start.X, p.X);
+            _xrectangle.Y = Math.Min(_start.Y, p.Y);
+            _xrectangle.Width = width;
+            _xrectangle.Height = height;
+            _rectangle.X = _xrectangle.X;
+            _rectangle.Y = _xrectangle.Y;
+            _rectangle.Width = _xrectangle.Width;
+            _rectangle.Height = _xrectangle.Height;
+        }
+
+        public void Dispose()
+        {
+            _downs.Dispose();
+            _drag.Dispose();
+        }
+    }
+
     #endregion
 
     #region WPF
@@ -601,6 +699,89 @@ namespace RxCanvas
         }
     }
 
+    public class WpfRectangle : XNative, IRectangle
+    {
+        private SolidColorBrush _strokeBrush;
+        private SolidColorBrush _fillBrush;
+        private Rectangle _rectangle;
+
+        public WpfRectangle(IRectangle rectangle)
+        {
+            _strokeBrush = new SolidColorBrush(Color.FromArgb(rectangle.Stroke.A, rectangle.Stroke.R, rectangle.Stroke.G, rectangle.Stroke.B));
+            _strokeBrush.Freeze();
+            _fillBrush = new SolidColorBrush(Color.FromArgb(rectangle.Fill.A, rectangle.Fill.R, rectangle.Fill.G, rectangle.Fill.B));
+            _fillBrush.Freeze();
+
+            _rectangle = new Rectangle()
+            {
+                Width = rectangle.Width,
+                Height = rectangle.Height,
+                Stroke = _strokeBrush,
+                StrokeThickness = rectangle.StrokeThickness,
+                Fill = _fillBrush
+            };
+
+            Canvas.SetLeft(_rectangle, rectangle.X);
+            Canvas.SetTop(_rectangle, rectangle.Y);
+
+            Native = _rectangle;
+        }
+
+        public double X
+        {
+            get { return Canvas.GetLeft(Native as Rectangle); }
+            set
+            {
+                Canvas.SetLeft(Native as Rectangle, value);
+            }
+        }
+
+        public double Y
+        {
+            get { return Canvas.GetTop(Native as Rectangle); }
+            set
+            {
+                Canvas.SetTop(Native as Rectangle, value);
+            }
+        }
+
+        public double Width
+        {
+            get { return (Native as Rectangle).Width; }
+            set { (Native as Rectangle).Width = value; }
+        }
+
+        public double Height
+        {
+            get { return (Native as Rectangle).Height; }
+            set { (Native as Rectangle).Height = value; }
+        }
+
+        public IColor Stroke
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public double StrokeThickness
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public IColor Fill
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public bool IsFilled
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+
     public class WpfCanvas : XNative, ICanvas
     {
         public IObservable<ImmutablePoint> Downs { get; set; }
@@ -668,6 +849,7 @@ namespace RxCanvas
     {
         ICanvas _canvas;
         IEditor _lineEditor;
+        IEditor _rectangleEditor;
         IEditor _quadraticBezierEditor;
 
         public MainWindow()
@@ -678,6 +860,11 @@ namespace RxCanvas
             Layout.Children.Add(_canvas.Native as UIElement);
 
             _lineEditor = new PortableXCanvasLineEditor(_canvas)
+            {
+                IsEnabled = false
+            };
+
+            _rectangleEditor = new PortableXCanvasRectangleEditor(_canvas)
             {
                 IsEnabled = false
             };
@@ -693,10 +880,17 @@ namespace RxCanvas
                 {
                     case Key.L:
                         _lineEditor.IsEnabled = true;
+                        _rectangleEditor.IsEnabled = false;
+                        _quadraticBezierEditor.IsEnabled = false;
+                        break;
+                    case Key.R:
+                        _lineEditor.IsEnabled = false;
+                        _rectangleEditor.IsEnabled = true;
                         _quadraticBezierEditor.IsEnabled = false;
                         break;
                     case Key.Q:
                         _lineEditor.IsEnabled = false;
+                        _rectangleEditor.IsEnabled = false;
                         _quadraticBezierEditor.IsEnabled = true;
                         break;
                 }
