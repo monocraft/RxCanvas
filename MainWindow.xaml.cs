b8e82bc023a63333a231eb8ae6c1d1fbe8a99903
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-06-23 22:31:48 +0200

    Added comments

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -232,22 +232,28 @@ namespace RxCanvas
 
         private void Open(string path, int index)
         {
+            // text
             var file = _drawingScope.Resolve<ITextFile>();
             var serializer = _serializers[index];
             var json = file.Open(path);
             var xcanvas = serializer.Deserialize(json);
             ConvertToNative(xcanvas);
+
+            // binary
             //var xcanvas = RxCanvas.Binary.BinaryConverter.Open(path);
             //ConvertToNative(xcanvas);
         }
 
         private void Save(string path, int index)
         {
+            // text
             var canvas = ConvertToModel();
             var file = _drawingScope.Resolve<ITextFile>();
             var serializer = _serializers[index];
             var json = serializer.Serialize(canvas);
             file.Save(path, json);
+
+            // binary
             //var drawingCanvas = _drawingScope.Resolve<ICanvas>();
             //RxCanvas.Binary.BinaryConverter.Save(path, drawingCanvas);
         }
